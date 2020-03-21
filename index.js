849ef7a7c1cf3e49f40a1b9afb943ae20b50d442
Cahyo Dwi Putro <BlackhatCode@users.noreply.github.com>
2020-03-21 17:30:04 +0800

    Added redux base structure

--- index.js
+++ index.js
@@ -2,8 +2,23 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import React from 'react';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { enableScreens } from 'react-native-screens';
+import configureStore from './redux/store/configureStore';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = configureStore()
+
+const RNRedux = () => (
+  <Provider store={ store }>
+    <App/>
+  </Provider>
+)
+
+enableScreens();
+console.disableYellowBox = true;
+
+AppRegistry.registerComponent(appName, () => RNRedux);
